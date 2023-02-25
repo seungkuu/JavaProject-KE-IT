2a5f24f937ed5da4e2618b060b212eb654bd75ef
kjaycek <seungkyukwon@koreanair.com>
2023-02-25 12:50:29 +0900

    commit for oneToHund prac

---
+++ oneToHund.java
@@ -0,0 +1,19 @@
+//public class oneToHund {
+//    public static void main(String[] args) {
+//
+//        for(int i = 1; i < 101; i++) {
+//            System.out.print(i + " ");
+//        }
+//    }
+//}
+
+public class oneToHund {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while(i < 101){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
